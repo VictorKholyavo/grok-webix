48d3b6c36edea63ad95eb2190b8fafa817544820
VictorKholyavo <44751406+VictorKholyavo@users.noreply.github.com>
2019-03-04 19:48:58 +0300

    Add files via upload

---
+++ sources/views/datasetA.js
@@ -0,0 +1,13 @@
+import {JetView} from "webix-jet";
+
+export default class ListView extends JetView{
+	config(){
+		return {
+			template:"datasetA",
+			css:"webix_shadow_medium app_start"
+		};
+	}
+	init(){
+
+	}
+}
---
+++ sources/views/settings.js
@@ -0,0 +1,13 @@
+import {JetView} from "webix-jet";
+
+export default class ListView extends JetView{
+	config(){
+		return {
+			template:"Settings",
+			css:"webix_shadow_medium app_start"
+		};
+	}
+	init(){
+
+	}
+}
